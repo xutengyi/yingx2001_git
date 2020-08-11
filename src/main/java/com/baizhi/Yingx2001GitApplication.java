0254eff75df2fe4550f866b4593e8c83e1c1730c
xutengyi <2735590371@qq.com>
2020-08-11 20:33:10 +0800

    这个项目的基础环境

---
+++ src/main/java/com/baizhi/Yingx2001GitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yingx2001GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (Yingx2001GitApplication.class, args);
+    }
+
+}
